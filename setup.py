5f64e7266c3163a7ff56421823f8827f16672eb9
potipot <p.potrykus@toucan-systems.pl>
2020-10-12 15:47:11 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fastai_audio',
+      version='0.1',
+      description='TAI development of community project fastai_audio',
+      url='https://github.com/potipot/fastai_audio',
+      author='potipot',
+      author_email='p.potrykus@toucan-systems.pl',
+      license='MIT',
+      packages=['fastai_audio'],
+      zip_safe=False)
